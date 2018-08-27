c4c3319e3fb33fc5d10a517c96022278c0916ba5
Unknown <sarai.gil.ramos12@gmail.com>
2018-08-26 19:20:06 -0500

    Actualizacion

--- js/main.js
+++ js/main.js
@@ -24,6 +24,15 @@
 		}, 600);
 	});
 
+	// Smooth scroll "Conoce Más" button
+	$("#contenidoPrinc .white-btn a[href^='#']").on('click', function(e) {
+		e.preventDefault();
+		var hash = this.hash;
+		$('html, body').animate({
+			scrollTop: $(this.hash).offset().top
+		}, 600);
+	});
+
 	$('#back-to-top').on('click', function(){
 		$('body,html').animate({
 			scrollTop: 0
